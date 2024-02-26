7c29e3d92ec0feca14d20fa2344b11548db4c80e
Grzegorz Buczyński <79482958+GrzegorzBuczynski@users.noreply.github.com>
2024-02-26 02:31:39 +0100

    Update threads.c

--- threads.c
+++ threads.c
@@ -4,7 +4,7 @@ void *funkcja_watku(void *arg) {
   // Kod wykonywany przez wątek
   for (int i = 0; i < 10; i++) {
     printf("Wątek: %d\n", i);
-    sleep(3);
+    sleep(1);
   }
   return NULL;
 }
