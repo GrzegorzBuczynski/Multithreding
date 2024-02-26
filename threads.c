e86260c83b5219460a224d4312f86ac577e2ae6e
Grzegorz Buczyński <79482958+GrzegorzBuczynski@users.noreply.github.com>
2024-02-26 02:21:10 +0100

    Create threads.c
    example for future use

---
+++ threads.c
@@ -0,0 +1,34 @@
+#include <pthread.h>
+
+void *funkcja_watku(void *arg) {
+  // Kod wykonywany przez wątek
+  for (int i = 0; i < 10; i++) {
+    printf("Wątek: %d\n", i);
+    sleep(3);
+  }
+  return NULL;
+}
+
+int main() {
+  pthread_t tid;
+  int err;
+  int err2;
+
+  // Tworzenie wątku
+  err = pthread_create(&tid, NULL, funkcja_watku, NULL);
+   err2 = pthread_create(&tid, NULL, funkcja_watku, NULL);
+  if (err != 0) {
+    printf("Błąd tworzenia wątku: %s\n", strerror(err));
+    return 1;
+  }
+    if (err2 != 0) {
+    printf("Błąd tworzenia wątku: %s\n", strerror(err));
+    return 1;
+  }
+
+  // Oczekiwanie na zakończenie wątku
+  pthread_join(tid, NULL);
+
+  printf("Wątek zakończony\n");
+  return 0;
+}
